b834ceb85205018ae0cbd8250703a004c3fefa4b
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-01-27 17:04:54 -0800

    Initial commit

---
+++ Populate/ACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ACAppDelegate.h
+//  Populate
+//
+//  Created by Arnaud Coomans on 27/01/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Populate/ACViewController.m
@@ -0,0 +1,29 @@
+//
+//  ACViewController.m
+//  Populate
+//
+//  Created by Arnaud Coomans on 27/01/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import "ACViewController.h"
+
+@interface ACViewController ()
+
+@end
+
+@implementation ACViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
