57b6ff8485be07182fd1907b2caee396732026c7
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-01-28 00:03:24 -0800

    Fixed completion blocks

--- PopulateKit/Populate.m
+++ PopulateKit/Populate.m
@@ -72,7 +72,7 @@
         }
         CFRelease(addressBookRef);
         
-        block();
+        if (block) block();
     });
 }
 
@@ -105,7 +105,7 @@
         }
         CFRelease(addressBookRef);
         
-        block();
+        if (block) block();
     });
 }
 
