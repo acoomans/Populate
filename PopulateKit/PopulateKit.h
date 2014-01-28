985713e439006a3a17e3a19979236230ba0f7650
Arnaud Coomans <arnaud.coomans@gmail.com>
2014-01-27 17:10:04 -0800

    Added PopulateKit static library target

---
+++ PopulateKit/PopulateKit.h
@@ -0,0 +1,13 @@
+//
+//  PopulateKit.h
+//  PopulateKit
+//
+//  Created by Arnaud Coomans on 27/01/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PopulateKit : NSObject
+
+@end
---
+++ PopulateKit/PopulateKit.m
@@ -0,0 +1,13 @@
+//
+//  PopulateKit.m
+//  PopulateKit
+//
+//  Created by Arnaud Coomans on 27/01/14.
+//  Copyright (c) 2014 Arnaud Coomans. All rights reserved.
+//
+
+#import "PopulateKit.h"
+
+@implementation PopulateKit
+
+@end
